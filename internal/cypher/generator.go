001e38a6a5f22e7c0b1ac0d445a2c57700811aac
Sean Condon <sean.condon@intel.com>
2025-09-01 19:30:47 +0100

    manual changes

--- internal/cypher/generator.go
+++ internal/cypher/generator.go
@@ -15,6 +15,9 @@ func Generate(resources []*parser.Resource, relationships []*relations.Relations
 	kinds := make(map[string]bool)
 	for _, r := range resources {
 		if _, ok := kinds[r.Kind]; !ok {
+			if r.Kind == "" {
+				continue
+			}
 			sb.WriteString(fmt.Sprintf("CREATE CONSTRAINT IF NOT EXISTS FOR (n:%s) REQUIRE (n.name, n.namespace) IS UNIQUE;\n", r.Kind))
 			kinds[r.Kind] = true
 		}
@@ -22,6 +25,9 @@ func Generate(resources []*parser.Resource, relationships []*relations.Relations
 
 	// Generate nodes
 	for _, r := range resources {
+		if r.Kind == "" {
+			continue
+		}
 		sb.WriteString(fmt.Sprintf("MERGE (:%s {name: '%s', namespace: '%s', kind: '%s'});\n", r.Kind, r.Metadata.Name, r.Metadata.Namespace, r.Kind))
 	}
 
